ac078278d7fca9e9b3bed119c1465f30b6e6d46d
baencho <yuen6482@gmail.com>
2025-07-03 15:22:05 +0900

    Mock 도입

--- DeviceDriver/main.cpp
+++ DeviceDriver/main.cpp
@@ -1,12 +1,25 @@
 #include "gmock/gmock.h"
 #include "device_driver.h"
+using namespace testing;
+
+class FlashMemoryDeviceMock : public FlashMemoryDevice {
+public:
+	MOCK_METHOD(unsigned char, read, (long address), (override));
+	MOCK_METHOD(void, write, (long address, unsigned char data), (override));
+};
 
 TEST(DeviceDriver, ReadFromHW) {
 	// TODO : replace hardware with a Test Double
-	FlashMemoryDevice* hardware = nullptr;
-	DeviceDriver driver{ hardware };
+	//FlashMemoryDevice* hardware = nullptr;
+	FlashMemoryDeviceMock hardware;
+	DeviceDriver driver{ &hardware };
+
+	EXPECT_CALL(hardware, read(_))
+		.Times(5)
+		.WillRepeatedly(Return(static_cast<unsigned char>('5')));
+
 	int data = driver.read(0xFF);
-	EXPECT_EQ(0, data);
+	EXPECT_EQ('5', data);
 }
 
 int main() {
